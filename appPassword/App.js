827d40b11539903bd4b89466080ee48c9a4b55fc
unknown <matheusfariasdasneves@gmail.com>
2023-11-11 20:55:07 -0300

    docs: adicionado imagem da logo no documento do projeto

--- appPassword/App.js
+++ appPassword/App.js
@@ -1,11 +1,16 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text>Open up App.js to start working on your app!</Text>
+      <Image
+        source={ require('./src/assets/logo.png') }
+        style={{ width: 200, height: 200 }}
+      />
     </View>
   );
 }
