f546320c635459d901399382e6d3aa377299c992
unknown <matheusfariasdasneves@gmail.com>
2023-11-13 18:00:36 -0300

    feat: criado componente ModalPassword

---
+++ appPassword/src/components/ModalPassword/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { 
+    Container 
+} from "./styled";
+
+export default function ModalPassword(){
+    return(
+        <Container>
+            
+        </Container>
+    );
+};
--- appPassword/src/pages/Home/index.js
+++ appPassword/src/pages/Home/index.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { Modal } from "react-native";
 import { 
     Container,
     Logo, 
@@ -10,7 +11,23 @@ import {
 
 import Slider from '@react-native-community/slider';
 
+
+let charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRESTUVWXYZ123456789';
+
 export default function Home(){
+    const [sliderValue, setSliderValue] = useState(10);
+    const [modalVisible, setModalVisible] = useState(true);
+
+    function generatePassword(){
+        let password = '';
+
+        for(let i = 0, n = charset.length; i < sliderValue.toFixed(0); i++){
+            password += charset.charAt(Math.floor(Math.random() * n));
+        };
+
+        alert(password);
+    };
+
     return(
         <Container>
             <Logo
@@ -18,19 +35,25 @@ export default function Home(){
             />
 
             <PasswordArea>
-                <PasswordTextLength>16 caracteres</PasswordTextLength>
+                <PasswordTextLength>{ sliderValue.toFixed(0) } caracteres</PasswordTextLength>
 
                 <Slider
                     style={{ width: '100%', height: 44 }}
                     minimumValue={ 6 }
                     maximumValue={ 20 }
                     thumbTintColor="#392DE4"
+                    value={ sliderValue }
+                    onValueChange={ (value) => setSliderValue(value) }
                 />
 
-                <Buttom>
+                <Buttom onPress={ generatePassword } >
                     <ButtonText>Gerar senha</ButtonText>
                 </Buttom>
             </PasswordArea>
+
+            <Modal visible={ modalVisible } transparent={ true } animationType='slide' >
+                <ButtonText>TEste</ButtonText>
+            </Modal>
         </Container>
     );
 };
